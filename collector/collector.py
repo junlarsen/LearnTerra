0f86c0239308fb743ce8115049542be1250ae71f
Michal Baumgartner <miso.baumgartner@gmail.com>
2019-11-18 19:06:00 +0100

    collector: store screenshots (windows only) (#21)

--- collector/collector.py
+++ collector/collector.py
@@ -8,6 +8,8 @@ import datetime
 import logging
 import uuid
 
+import d3dshot
+
 logger = logging.getLogger('collector')
 
 logging.basicConfig(
@@ -21,6 +23,9 @@ PORT = 21337
 START_ID = str(uuid.uuid4())
 GAME_ID = START_ID
 GAME_STATE = None
+STARTED_AT = None
+
+screenshot_producer = d3dshot.create()
 
 
 def every(delay, task):
@@ -36,7 +41,7 @@ def every(delay, task):
 
 
 def poll_lor_api():
-    global GAME_STATE, GAME_ID
+    global GAME_STATE, GAME_ID, STARTED_AT
 
     try:
         response = requests.get(f'http://localhost:{PORT}/positional-rectangles', timeout=1)
@@ -45,7 +50,9 @@ def poll_lor_api():
         if data['GameState'] == 'Menus':
             GAME_STATE = data['GameState']
 
-        if not data['Rectangles']:
+        if not data['Rectangles'] and data['GameState'] != 'InProgress':
+            # screenshot_producer.stop()
+
             logger.info(f'Received empty response: {data}')
             return
     except (ConnectionRefusedError, requests.exceptions.ConnectionError):
@@ -54,16 +61,76 @@ def poll_lor_api():
 
     previous_state = GAME_STATE
     GAME_STATE = data['GameState']
+
     if GAME_STATE == 'InProgress' and previous_state != GAME_STATE:
         GAME_ID = str(uuid.uuid4())
-        logger.info(f'Detected a new match, setting game_id={GAME_ID}')
+        STARTED_AT = datetime.datetime.now()
+        logger.info(f'Detected a new match at {STARTED_AT}, setting game_id={GAME_ID}')
 
-    file_name = f'.data/{GAME_ID}/{int(datetime.datetime.now().timestamp())}.json'
+        screenshot_dir = f'.data/{GAME_ID}/capture'
+        os.makedirs(screenshot_dir, exist_ok=True)
+        # screenshot_producer.screenshot_to_disk_every(1, directory=screenshot_dir)
+
+    game_length = datetime.datetime.now() - STARTED_AT
+
+    file_name = f'.data/{GAME_ID}/{int(game_length.total_seconds())}.json'
+    screenshot_file_name = f'.data/{GAME_ID}/capture/{int(game_length.total_seconds())}.png'
+
     os.makedirs(os.path.dirname(file_name), exist_ok=True)
 
     with open(file_name, 'w') as data_file:
         data_file.write(response.text)
         logger.info(f'Request succeeded, saving to {file_name}')
+
+    screenshot(screenshot_file_name)
+
+
+def screenshot(file_name, window_name='Legends of Runeterra'):
+    # https://stackoverflow.com/questions/19695214/python-screenshot-of-inactive-window-printwindow-win32gui
+    import win32gui
+    import win32ui
+    from ctypes import windll
+    from PIL import Image
+
+    hwnd = win32gui.FindWindow(None, window_name)
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    #left, top, right, bot = win32gui.GetClientRect(hwnd)
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w = right - left
+    h = bot - top
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+    saveDC.SelectObject(saveBitMap)
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    #result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer(
+        'RGB',
+        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+        bmpstr, 'raw', 'BGRX', 0, 1)
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        logger.info(f'Saving screenshot to {file_name}')
+        im.save(file_name)
 
 
 if __name__ == '__main__':
