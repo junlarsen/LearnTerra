b48f9015b015c8ba8b111c48500f9dc7db15e0ba
supergrecko <me@supergrecko.com>
2019-11-20 16:34:12 +0100

    frontend: make game board layout

--- frontend/src/components/board/index.tsx
+++ frontend/src/components/board/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
-import { BoardWrapper } from './styles'
+import { BoardWrapper, BoardRow, GameBoard, Card, InfoBox } from './styles'
 
 export function Board() {
   return (
-    <BoardWrapper/>
+    <BoardWrapper>
+      {/* TODO: make board thinner, add tags / game nexus */}
+      <InfoBox>
+        All active annotations for this frame probably
+      </InfoBox>
+
+      <GameBoard>
+        <BoardRow className="dark"></BoardRow>
+        <BoardRow className="light"></BoardRow>
+        <BoardRow className="light"></BoardRow>
+        <BoardRow className="dark">
+          <Card>Yes</Card>
+        </BoardRow>
+      </GameBoard>
+    </BoardWrapper>
   )
 }
--- frontend/src/components/board/styles.ts
+++ frontend/src/components/board/styles.ts
@@ -3,4 +3,44 @@ import styled from 'styled-components'
 export const BoardWrapper = styled.div`
   background-color: #c1d5e0;
   height: calc(100vh - 64px - 48px);
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+`
+
+export const InfoBox = styled.div`
+  width: 10vw;
+`
+
+export const GameBoard = styled.div`
+  height: inherit;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+export const Card = styled.div`
+  width: calc(16.667% - 3rem);
+  height: calc(100% - 3rem);
+  background: ${props => props.theme.color.light};
+  
+  margin: 1.5rem;
+`
+
+export const BoardRow = styled.div`
+  width: 100%;
+  height: 25%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: center;
+  
+  &.dark {
+    background: ${props => props.theme.color.dark};
+  }
+  
+  &.light {
+    background: ${props => props.theme.color.primary};
+  }
 `
